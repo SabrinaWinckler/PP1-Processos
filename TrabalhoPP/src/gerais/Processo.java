438c2e728b3fec91e0adac948b30c554875f0364
Lucas Martins <lgatts@gmail.com>
2017-08-11 13:59:33 -0300

    Processo
    Adicionada Classe Processo

--- TrabalhoPP/src/gerais/Processo.java
+++ TrabalhoPP/src/gerais/Processo.java
@@ -10,5 +10,29 @@ package gerais;
  * @author Lucas
  */
 public class Processo {
+
+    private int tempo;
+    private int prioridade;
+    private boolean finalizado;
+
+    public void Processo(int tempo, int prioridade) {
+        this.tempo = tempo;
+        this.prioridade = prioridade;
+        this.finalizado = false;
+    }
+
+    public void executar() {
+        if (this.tempo > 0) {
+            this.tempo--;
+            if (this.tempo == 0) {
+                this.finalizado = true;
+            }
+
+        }
+    }
+
+    public boolean estaFinalizado(){
+        return this.finalizado;
+    }
     
 }
