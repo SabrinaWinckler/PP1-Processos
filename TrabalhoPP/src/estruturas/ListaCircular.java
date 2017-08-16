1ca7a216dca37f7f52445558553833bfe23fc032
Lucas Martins <lgatts@gmail.com>
2017-08-15 21:47:15 -0300

    Lista Circular Terminada
    Falta Testar

--- TrabalhoPP/src/estruturas/ListaCircular.java
+++ TrabalhoPP/src/estruturas/ListaCircular.java
@@ -10,38 +10,74 @@ package estruturas;
  * @author Lucas
  */
 public class ListaCircular<T> {
-    
+
+    //<editor-fold defaultstate="collapsed" desc="Declaração Variaveis">
     Object[] objetos;
-    
+
     int tamanho;
     int posicaoAtual;
-    int posicoesLivres;
-    
-    public ListaCircular(int tamanho){
+    int posicoesPreenchidas;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Construtor">
+    public ListaCircular(int tamanho) {
         this.tamanho = tamanho;
         this.objetos = new Object[tamanho];
     }
-    
-    public void resetar(){
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Metodos Publicos">
+    public void resetar() {
         this.posicaoAtual = 0;
-        this.posicoesLivres = 0;
+        this.posicoesPreenchidas = 0;
     }
-    
-    public boolean inserir(T objeto ){
-        this.objetos[0] = objeto;
-        return true;
+
+    public boolean inserir(T objeto) {
+
+        if (posicoesPreenchidas < tamanho) {
+            if (posicaoAtual >= tamanho) {
+                posicaoAtual = 0;
+            }
+            objetos[posicaoAtual] = objeto;
+            posicaoAtual++;
+            posicoesPreenchidas++;
+            return true;
+        }
+
+        return false;
     }
+
+    public T obter() {
+        if (posicoesPreenchidas == 0) {
+            return null;
+        } else {
+            int proximo = posicaoAtual - posicoesPreenchidas;
+            if (proximo < 0) {
+                proximo += tamanho;
+            }
+            T objeto = (T) this.objetos[proximo];
+            posicoesPreenchidas--;
+            return objeto;
+        }
+    }
+
+    //</editor-fold>
     
-    public int getTamanho(){
+    //<editor-fold defaultstate="collapsed" desc="Gets">
+    public int getTamanho() {
         return this.tamanho;
     }
-    
-    public int getPosicaoAtual(){
+
+    public int getPosicaoAtual() {
         return this.posicaoAtual;
     }
-    
-    public int getPosicoesLivres(){
-        return this.posicoesLivres;
-    }
-}
 
+    public int getTamanhoAtual() {
+        return this.posicoesPreenchidas;
+    }
+
+    public int getDisponivel() {
+        return this.tamanho - this.posicoesPreenchidas;
+    }
+    //</editor-fold>
+}
