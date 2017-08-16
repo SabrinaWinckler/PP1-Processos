787e6cc9265db234e040383dbac628f39c101ede
Gustavo Satheler <gustavosatheler@gmail.com>
2017-08-16 16:43:59 -0300

    Lista encadeada (completo) e SJF (vou refazer)

--- TrabalhoPP/src/escalonadores/SJF.java
+++ TrabalhoPP/src/escalonadores/SJF.java
@@ -5,10 +5,64 @@
  */
 package escalonadores;
 
+import estruturas.ListaEncadeada;
+import gerais.Processo;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
- * @author Lucas
+ * @author Gustavo Bittencourt Satheler
+ * <gustavo.satheler@alunos.unipampa.edu.br>
  */
 public class SJF {
-    
+
+    private int numeroDeProcessos;
+
+    private ListaEncadeada processos = new ListaEncadeada();
+    private ListaEncadeada naFila = new ListaEncadeada();
+    private ListaEncadeada foraDaFila = new ListaEncadeada();
+
+    public void adicionarProcesso(Processo meuProcesso) {
+        if (meuProcesso.getChegada() == 0) {
+            naFila.adicionar(meuProcesso);
+        } else {
+            foraDaFila.adicionar(meuProcesso);
+        }
+        numeroDeProcessos++;
+    }
+
+    public void organizar() {
+        int tempo = 0;
+        int iniciarProcesso = 0;
+        
+        Processo agora = null;
+        Processo novo = null;
+        do {
+            tempo += 1;
+
+            if (naFila.tamanho() != 0) {
+                agora = (Processo) naFila.pegar(0);
+
+                if (agora.getTempo() == 1) {
+                    System.out.println(agora.getNome() + " executou entre o tempo " + iniciarProcesso + " e " + tempo);
+                    iniciarProcesso = tempo;
+                    naFila.remover(0);
+                } else {
+                    agora.executar();
+                }
+            }
+            if (foraDaFila.tamanho() != 0) {
+                novo = (Processo) foraDaFila.pegar(0);
+                if (novo.getChegada() == tempo) {
+                    if (agora.getTempo() > novo.getTempo()) {
+                        System.out.println(agora.getNome() + " executou entre o tempo " + iniciarProcesso + " e " + tempo);
+                        iniciarProcesso = tempo;
+                    }
+                    naFila.adicionar(novo.clonar());
+                    foraDaFila.remover(0);
+                }
+            }
+        } while (foraDaFila.tamanho() != 0 || naFila.tamanho() != 0);
+    }
 }
