bb10a7edee0bcfe768f882d6e5b6e3c1f3178507
Lucas Martins <lgatts@gmail.com>
2017-08-19 09:32:55 -0300

    Alteração RR e GitIgnore

--- TrabalhoPP/src/escalonadores/RR.java
+++ TrabalhoPP/src/escalonadores/RR.java
@@ -7,20 +7,65 @@ package escalonadores;
 
 import estruturas.ListaCircular;
 import gerais.Processo;
+import java.util.ArrayList;
 
 /**
  *
  * @author Lucas
  */
 public class RR {
-    
-    ListaCircular<Processo> listaProcessos;
-    
-    public RR(){
-        
+
+    private ListaCircular<Processo> listaProcessos;
+    private ArrayList<Processo> listaEntrada;
+    private int tamanhoQuantum;
+
+    public RR(ArrayList listaEntrada, int tamanhoQuantum) {
+
+        int quantidadeProcessos;
+
+        this.listaEntrada = listaEntrada;
+        this.tamanhoQuantum = tamanhoQuantum;
+        quantidadeProcessos = listaEntrada.size();
+
+        this.listaProcessos = new ListaCircular(quantidadeProcessos);
+
     }
-    
-    
-    
-    
+
+    public void executar() {
+
+        int tempoClock = 0;
+        int quantum = 0;
+
+        while (listaProcessos.getTamanho() > 0 && listaEntrada.size() > 0) {
+
+            for (int i = 0; i < listaEntrada.size(); i++) {
+                if (listaEntrada.get(i).getChegada() == tempoClock) {
+                    listaProcessos.inserir(listaEntrada.remove(i));
+                }
+            }
+            
+            if(listaProcessos.getTamanho() > 0){
+                listaProcessos.obter().executar();
+            }
+            
+            if(listaProcessos.obter().getTempo() == 0){
+                
+            }
+            
+            quantum++;
+            tempoClock++;
+            
+            if(quantum == this.tamanhoQuantum){
+                quantum = 0;
+            }
+            
+            if(quantum == 0){
+//                listaProcessos.;
+            }
+            
+
+        }
+
+    }
+
 }
--- TrabalhoPP/src/estruturas/ListaCircular.java
+++ TrabalhoPP/src/estruturas/ListaCircular.java
@@ -14,70 +14,68 @@ public class ListaCircular<T> {
     //<editor-fold defaultstate="collapsed" desc="Declaração Variaveis">
     Object[] objetos;
 
-    int tamanho;
+    int tamanhoMaximo;
+    int tamanhoAtual;
+    int posicaoEscrita;
     int posicaoAtual;
-    int posicoesPreenchidas;
-    //</editor-fold>
 
+    //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="Construtor">
-    public ListaCircular(int tamanho) {
-        this.tamanho = tamanho;
-        this.objetos = new Object[tamanho];
+    public ListaCircular(int tamanhoMaximo) {
+        this.tamanhoMaximo = tamanhoMaximo;
+        this.objetos = new Object[tamanhoMaximo];
+        this.tamanhoAtual = 0;
+        this.posicaoEscrita = 0;
+        this.posicaoAtual = 0;
     }
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Metodos Publicos">
     public void resetar() {
-        this.posicaoAtual = 0;
-        this.posicoesPreenchidas = 0;
+        this.posicaoEscrita = 0;
+        this.tamanhoAtual = 0;
     }
 
     public boolean inserir(T objeto) {
 
-        if (posicoesPreenchidas < tamanho) {
-            if (posicaoAtual >= tamanho) {
-                posicaoAtual = 0;
-            }
-            objetos[posicaoAtual] = objeto;
-            posicaoAtual++;
-            posicoesPreenchidas++;
-            return true;
+        if (posicaoEscrita == tamanhoMaximo) {
+            posicaoEscrita = 0;
         }
-
-        return false;
+        objetos[posicaoEscrita] = objeto;
+        posicaoEscrita++;
+        tamanhoAtual++;
+        return true;
+        
     }
 
-    public T obter() {
-        if (posicoesPreenchidas == 0) {
-            return null;
-        } else {
-            int proximo = posicaoAtual - posicoesPreenchidas;
-            if (proximo < 0) {
-                proximo += tamanho;
-            }
-            T objeto = (T) this.objetos[proximo];
-            posicoesPreenchidas--;
-            return objeto;
-        }
+    public T obter() {        
+      
+        return (T) objetos[posicaoAtual];
+        
+    }
+    
+    public T remover(){
+        
+        T objeto = (T) objetos[posicaoAtual];
+        
+        objetos[posicaoAtual] = null;
+        
+        
+        
+        
+        return objeto;
+        
+        
     }
 
     //</editor-fold>
-    
     //<editor-fold defaultstate="collapsed" desc="Gets">
     public int getTamanho() {
-        return this.tamanho;
+        return this.tamanhoAtual;
     }
 
     public int getPosicaoAtual() {
         return this.posicaoAtual;
-    }
-
-    public int getTamanhoAtual() {
-        return this.posicoesPreenchidas;
-    }
-
-    public int getDisponivel() {
-        return this.tamanho - this.posicoesPreenchidas;
-    }
+    }   
     //</editor-fold>
 }
