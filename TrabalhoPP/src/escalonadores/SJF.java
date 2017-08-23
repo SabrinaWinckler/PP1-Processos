adfbeeb793d1be3aa62074d505c9b23119c1e5ea
Gustavo Satheler <gustavosatheler@gmail.com>
2017-08-22 21:08:48 -0300

    SJF (com alguns bug's)

--- TrabalhoPP/src/escalonadores/SJF.java
+++ TrabalhoPP/src/escalonadores/SJF.java
@@ -7,6 +7,9 @@ package escalonadores;
 
 import estruturas.ListaEncadeada;
 import gerais.Processo;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  *
@@ -19,8 +22,6 @@ public class SJF {
 
     private ListaEncadeada emExecucao = new ListaEncadeada();
     private ListaEncadeada emEspera = new ListaEncadeada();
-    
-    private ListaEncadeada organizador = new ListaEncadeada();
 
     public void adicionarProcesso(Processo processo) {
         if (processo.getChegada() == 0) {
@@ -33,33 +34,92 @@ public class SJF {
 
     public void executar() {
         int tempo = 0;
+        int tempoInicial = 0;
+        int tique = 0;
+
+        ordenarDuracao(emExecucao);
+        ordenarDuracao(emEspera);
+        ordenarChegada(emEspera);
+
+//        Collections.sort(inQueue, BusrtCom);
+//        Collections.sort(Not_In_Queue_Yet, BusrtCom_Arr);
+        Processo processoAtual = null;
+        Processo novoProcesso = null;
 
         do {
-            if (emExecucao.vazio() && !emEspera.vazio()) {
-                for(int i = 0; i < emEspera.tamanho() - 1; i++){
-                                        
-                    Processo processo1 = (Processo) emEspera.pegar(i);
-                    Processo processo2 = (Processo) emEspera.pegar(i + 1);
-                    
-                    if (processo1.getChegada() == tempo && processo2.getChegada() == tempo){
-                        int calc = processo1.getTempo() - processo2.getTempo();
-                        
-                        if (calc > 0){
-                            emExecucao.adicionar(processo1);
-                        } else if (calc < 0) {
-                            emExecucao.adicionar(processo2);
-                        } else {
-                            emExecucao.adicionar(processo1);
-                        }
+            tempo += 1;
+
+            if (!emExecucao.vazio()) {
+                processoAtual = (Processo) emExecucao.pegar(0);
+
+                if (processoAtual.getTempo() == 1) {
+                    for(int i = 0; i < (tempo - tempoInicial); i++){
+                        System.out.println(processoAtual.getNome() + " " + tique);
+                        tique++;
                     }
+                    tempoInicial = tempo;
+                    emExecucao.remover(0);
+                } else {
+                    processoAtual.executar();
                 }
-                
-//                emExecucao.adicionar(emEspera.pegar(0));
-            } else {
-                
             }
-            
-            tempo++;
-        } while (emExecucao.tamanho() != 0 || emEspera.tamanho() != 0);
+
+            if (!emEspera.vazio()) {
+                novoProcesso = (Processo) emEspera.pegar(0);
+                if (novoProcesso.getChegada() == tempo) {
+                    if (processoAtual.getTempo() > novoProcesso.getTempo()) {
+                        for(int i = 0; i < (tempo - tempoInicial); i++){
+                            System.out.println(processoAtual.getNome() + " " + tique);
+                            tique++;
+                        }
+                        tempoInicial = tempo;
+                    }
+
+                    emExecucao.adicionarNoComeco(new Processo(novoProcesso.getNome(), novoProcesso.getChegada(), novoProcesso.getTempo()));
+                    emEspera.remover(0);
+                    ordenarDuracao(emExecucao);
+                }
+            }
+
+        } while (!emExecucao.vazio() || !emEspera.vazio());
+    }
+
+    private ListaEncadeada ordenarDuracao(ListaEncadeada lista1) {
+        ArrayList<Processo> lista2 = new ArrayList();
+        while (!lista1.vazio()) {
+            lista2.add((Processo) lista1.pegar(0));
+            lista1.remover(0);
+        }
+
+        Collections.sort(lista2, (o1, o2) -> {
+            return ((Processo) o1).getTempo() - ((Processo) o2).getTempo();
+
+        });
+
+        while(!lista2.isEmpty()){
+            lista1.adicionar(lista2.get(0));
+            lista2.remove(0);
+        }
+
+        return lista1;
+    }
+    
+        private ListaEncadeada ordenarChegada(ListaEncadeada lista1) {
+        ArrayList<Processo> lista2 = new ArrayList();
+        while (!lista1.vazio()) {
+            lista2.add((Processo) lista1.pegar(0));
+            lista1.remover(0);
+        }
+        
+        Collections.sort(lista2, (o1, o2) -> {
+            return ((Processo) o1).getChegada() - ((Processo) o2).getChegada();
+        });
+
+        while(!lista2.isEmpty()){
+            lista1.adicionar(lista2.get(0));
+            lista2.remove(0);
+        }
+
+        return lista1;
     }
 }
